cbd18eac85169a189dce8c1450d89b6760a766b6
AbenezerDer <dabenu2012@gmail.com>
2022-09-08 10:12:18 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#includ <stdio.h>
+
+/**
+* main - print the string in the put function 
+*
+* Description: using the main function
+* this program prints "programing is like building a multilingual puzzel"
+* Return:0
+*/
+int main(viod)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
