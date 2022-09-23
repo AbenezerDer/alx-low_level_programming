266060303919f32e5c7e69d161a77510ded13734
AbenezerDer <dabenu2012@gmail.com>
2022-09-23 04:36:55 -0700

    main.c file

---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - call other functions
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char *str = "I am a string";
+
+	stringManipulation(str);
+
+	return (0);
+}
