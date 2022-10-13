8c57056c9d0cbfef23a7eaaefbeabdd82d594f43
AbenezerDer <dabenu2012@gmail.com>
2022-10-13 03:52:55 -0700

    hot fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,6 +33,9 @@ void print_all(const char * const format, ...)
 		case 'i':
 			printf("%d", va_arg(valist, int)), c = 1;
 			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
 		case 's':
 			str = va_arg(valist, char *), c = 1;
 			if (!str)
