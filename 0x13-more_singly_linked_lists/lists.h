a55d7d8565ee7c6cf4d19ceb41851bfdc1cffe84
AbenezerDer <dabenu2012@gmail.com>
2022-10-25 14:55:32 -0700

    lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,6 +14,19 @@
  */
 typedef struct listint_s
 {
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
 	void *p;
 	struct listp_s *next;
 } listp_t;
